6cdfd0cb0669700ad6fc71d5679316c41cfebaf2
FE Juniors <john@ewave.com>
2017-03-13 18:12:38 +0300

    #HomePage: add some markup

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,13 +27,16 @@ module.exports = function (grunt) {
             }
         },
         watch: {
+            options: {
+                livereload: true,
+            },
             html: {
                 files: ["src/*.html"],
                 tasks: ["copy:main"]
             },
             style: {
                 files: ["src/css/**/*.less"],
-                tasks: ["less","postcss","csso"]
+                tasks: ["less"]
             }
         },
         browserSync: {
